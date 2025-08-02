255b3b7cf559fe587971a91bfb7b9e4dfdb74459
SocketByte <poczta.xvacuum@gmail.com>
2025-08-02 03:04:49 +0200

    WSS-JS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import { useEffect } from "react";
+import { ShellIPC } from "wss-js";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    ShellIPC.connect("ws://localhost:8080").then((r) => {
+      if (r) {
+        console.log("Connected to Shell IPC");
+      } else {
+        console.error("Failed to connect to Shell IPC");
+      }
+    });
+  }, []);
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
